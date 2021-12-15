2b57c9ccd093504f954d3b00d5b33ce11328b0c8
gadlaik <sabatonz@hotmail.com>
2021-12-16 00:14:13 +0100

    Home page mostly complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import WaldoHi from "./assets/WaldoHi.png";
+import whereWaldo from "./assets/whereWaldoSM.jpg";
+import whereWaldo2 from "./assets/whereWaldo2SM.jpg";
+import whereWaldo3 from "./assets/whereWaldo3SM.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="home-header">
+        <h1>Where's Waldo?</h1>
+        <img src={WaldoHi} alt="Waldo Wave" />
       </header>
+
+      <main className="levels">
+        <div className="level">
+          <img src={whereWaldo} alt="level 2 preview" />
+          <div>
+            <h3>Level 1</h3>
+          </div>
+        </div>
+        <div className="level">
+          <img src={whereWaldo2} alt="level 2 preview" />
+          <div>
+            <h3>Level 2</h3>
+          </div>
+        </div>
+        <div className="level">
+          <img src={whereWaldo3} alt="level 3 preview" />
+          <div>
+            <h3>Level 3</h3>
+          </div>
+        </div>
+      </main>
+
+      <footer>Property of Gadlajk</footer>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGSxjN_mkql1jXyVtOsOZvLxhpFQubtqk",
+  authDomain: "where-is-waldo-fb394.firebaseapp.com",
+  projectId: "where-is-waldo-fb394",
+  storageBucket: "where-is-waldo-fb394.appspot.com",
+  messagingSenderId: "1010758292102",
+  appId: "1:1010758292102:web:0d3e38ad35c11531e13707",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
