ad81a68b5d991e635366c4cfa14d094fdefccb74
gadlaik <sabatonz@hotmail.com>
2021-12-20 18:41:07 +0100

    Dropdown hide/show implemented, firebase added

--- src/components/MatrixGrid.js
+++ src/components/MatrixGrid.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-function MatrixGrid() {
+function MatrixGrid(e) {
   useEffect(() => {
     for (let i = 0; i < 330; i++) {
       let cube = document.createElement("div");
@@ -8,7 +8,42 @@ function MatrixGrid() {
       document.querySelector(".matrix").append(cube);
     }
   }, []);
-  return <div className="matrix"></div>;
+
+  function foundWaldo(e) {
+    if (
+      e.target.className === "matrix" ||
+      e.target.parentElement.className === "matrix"
+    ) {
+      if (
+        e.target.className === "fragment64" ||
+        e.target.className === "fragment65"
+      )
+        document.querySelector(".waldo").style.opacity = 0.3;
+      else console.log(false);
+
+      console.log(e);
+      document.querySelector(".dropdown").style.top = `${e.clientY - 120}px`;
+      document.querySelector(".dropdown").style.left = `${e.clientX - 40}px`;
+      document.querySelector(".dropdown").style.display = "block";
+    }
+  }
+
+  return (
+    <div className="matrix" onClick={foundWaldo}>
+      <div
+        className="dropdown"
+        style={{
+          position: "absolute",
+          height: 30,
+          width: 100,
+          backgroundColor: "yellow",
+          display: "none",
+        }}
+      >
+        <p>Waldo</p>
+      </div>
+    </div>
+  );
 }
 
 export default MatrixGrid;
--- src/pages/Level1.js
+++ src/pages/Level1.js
@@ -3,19 +3,21 @@ import whereWaldo from "../assets/whereWaldo.jpg";
 import "../styles/Level.css";
 import waldo from "../assets/Waldo.png";
 import MatrixGrid from "../components/MatrixGrid";
+import { collection, getDocs } from "firebase/firestore/lite";
+import db from "../firebase";
 
 function Level1() {
-  function foundWaldo(e) {
-    if (
-      e.target.className === "fragment64" ||
-      e.target.className === "fragment65"
-    )
-      document.querySelector(".waldo").style.opacity = 0.3;
-    else console.log(false);
-  }
-
   return (
-    <div className="level-container" onClick={foundWaldo}>
+    <div
+      className="level-container"
+      onClick={(e) => {
+        if (
+          e.target.className !== "matrix" &&
+          e.target.parentElement.className !== "matrix"
+        )
+          document.querySelector(".dropdown").style.display = "none";
+      }}
+    >
       <div className="home-header level-header">
         <h1>Where's</h1>
         <img src={waldo} alt="Waldo Wave" className="waldo" />
