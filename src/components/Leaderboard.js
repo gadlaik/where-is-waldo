def60cc90a128293a3731db308508b94b1570246
gadlaik <sabatonz@hotmail.com>
2022-01-01 00:55:17 +0100

    Project mostly complete

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,38 @@
-function Leaderboard({ leaderboard }) {
-  return <div className="leaderboard">top 10:</div>;
+import { useEffect, useState } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import db from "../firebase";
+import "../styles/leaderboard.css";
+
+function Leaderboard() {
+  let [leaderboard, setLeaderboard] = useState();
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "highscore"), (snapshot) => {
+        setLeaderboard(
+          snapshot.docs
+            .map((doc) => doc.data())
+            .sort((a, b) => a.value - b.value)
+        );
+      }),
+    []
+  );
+
+  if (leaderboard) {
+    document.querySelector(".leaderboard > ol").innerHTML = "";
+    leaderboard.forEach((score, i) => {
+      document.querySelector(".leaderboard > ol").innerHTML += `<li>${
+        score.name
+      } ${" "}<span>time: ${score.value}<span></li>`;
+    });
+  }
+
+  return (
+    <div className="leaderboard">
+      <h1>TOP SCORES</h1>
+      <ol></ol>
+    </div>
+  );
 }
 
 export default Leaderboard;
